02d6fbe4e4a91a48abe9c53b2a99edac7f2a8783
kevin <1719451186@qq.com>
2025-08-28 23:23:19 +0800

    新增：2.5冒泡排序代码

---
+++ 从零开始学算法&数据结构/常用查找与排序算法/bubble_sort.py
@@ -0,0 +1,33 @@
+
+
+import random
+
+# 升序 list[j] > list[j+1]
+# 降序 list[j] < list[j+1]
+
+# 初始版本
+def bubble_sort(list):
+    for i in range(len(list)-1):
+        for j in range(len(list)-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j] # 交换
+
+# 改进版本
+def bubble_sort(list):
+    for i in range(len(list)-1):
+        if_exchange = False
+        for j in range(len(list)-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j] # 交换
+                if_exchange = True  
+        if not if_exchange: # 如果没有交换，说明已经有序，直接退出
+            return 
+
+
+# 验证
+list1 = [random.randint(1, 1000) for _ in range(10)]
+print(list1)
+bubble_sort(list1)
+print(list1)
+
+
