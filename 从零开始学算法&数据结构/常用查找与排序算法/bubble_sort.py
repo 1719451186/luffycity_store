806a55cf727ba6bbb988ac0096b7dfc7e30956a6
wbooy-thinkbook <wboy1999@outlook.com>
2025-08-29 16:08:27 +0800

    修改： 新添了两个正序和倒序排序的实验

--- 从零开始学算法&数据结构/常用查找与排序算法/bubble_sort.py
+++ 从零开始学算法&数据结构/常用查找与排序算法/bubble_sort.py
@@ -13,11 +13,11 @@ def bubble_sort(list):
                 list[j], list[j+1] = list[j+1], list[j] # 交换
 
 # 改进版本
-def bubble_sort(list):
+def bubble_sort_2(list):
     for i in range(len(list)-1):
         if_exchange = False
         for j in range(len(list)-i-1):
-            if list[j] > list[j+1]:
+            if list[j] < list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j] # 交换
                 if_exchange = True  
         if not if_exchange: # 如果没有交换，说明已经有序，直接退出
@@ -26,8 +26,15 @@ def bubble_sort(list):
 
 # 验证
 list1 = [random.randint(1, 1000) for _ in range(10)]
+list2 = [random.randint(1, 1000) for _ in range(10)]
 print(list1)
+print(list2)
+
+print("------------------")
+# 正序 倒序实验
 bubble_sort(list1)
+bubble_sort_2(list2)
 print(list1)
+print(list2)
 
 
