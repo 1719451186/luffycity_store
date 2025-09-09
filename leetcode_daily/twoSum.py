910a601b5af033df12b23fa223d0d79fb76903bd
kevin <1719451186@qq.com>
2025-09-09 21:17:14 +0800

    新增：twoSum

---
+++ leetcode_daily/twoSum.py
@@ -0,0 +1,12 @@
+# 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
